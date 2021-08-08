07cdceb4ced4e6e7e63f296ffd954127834a3252
Nelson Nelson-Atuonwu <nellyatuonwu@gmail.com>
2021-08-08 17:24:20 +0100

    Update build-svg.js

--- build-svg.js
+++ build-svg.js
@@ -49,7 +49,7 @@ const psTime = formatDistance(new Date(2019, 08, 28), today, {
 })
 
 // Today's weather
-const locationKey = '347629'
+const locationKey = '254085'
 let url = `forecasts/v1/daily/1day/${locationKey}?apikey=${WEATHER_API_KEY}`
 
 got(url, { prefixUrl: WEATHER_DOMAIN })
